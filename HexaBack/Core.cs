f7b32128c7148e8ada88ceb58d41bf1f027ff2eb
freaky cheesy <92267915+freakycheesy@users.noreply.github.com>
2025-08-09 23:56:28 +0100

    removed some jank and added bonemenu

--- HexaBack/Core.cs
+++ HexaBack/Core.cs
@@ -1,4 +1,5 @@
 ﻿using BoneLib;
+using BoneLib.BoneMenu;
 using HarmonyLib;
 using Il2CppSLZ.Marrow;
 using MelonLoader;
@@ -11,34 +12,103 @@ namespace HexaBack
 {
     public class Core : MelonMod
     {
-        public override void OnInitializeMelon()
-        {
+        public static Page page;
+        public static MelonPreferences_Category category;
+        public static MelonPreferences_Entry<bool> modEnabled;
+        public override void OnInitializeMelon() {
             LoggerInstance.Msg("Initialized HexaBack.");
+            CreatePrefs();
+            CreateBoneMenu();
             Hooking.OnLevelLoaded += Hooking_OnMarrowGameStarted;
+        }
+
+        private static void CreatePrefs() {
+            category = MelonPreferences.CreateCategory("Hexaback");
+            category.SetFilePath("UserData/freakycheesy.cfg", true);
+            modEnabled = category.CreateEntry("Enabled", true);
+        }
+
+        private void CreateBoneMenu() {          
+            page = Page.Root.CreatePage("Hexaback", Color.green);
+            page.CreateBool("Toggle Mod (Works on Level Load)", Color.green, modEnabled.Value, (a) => {
+                modEnabled.Value = a;
+                Save();
+            });
+        }
+
+        private static void Save() {
+            category.SaveToFile();
+            MelonPreferences.Save();
+        }
+
+        public override void OnApplicationQuit() {
+            MelonPreferences.Save();
+            base.OnApplicationQuit();
         }
 
         private void Hooking_OnMarrowGameStarted(LevelInfo info) {
             MakeHeptabodyHexabody(Player.PhysicsRig);
         }
+
         private static void MakeHeptabodyHexabody(PhysicsRig rig) {
             var lShoulder = rig.m_shoulderLf.GetComponent<Collider>();
             var rShoulder = rig.m_shoulderRt.GetComponent<Collider>();
             var lElbow = rig.m_elbowLf.GetComponent<Collider>();
-            var rElbow = rig.m_elbowRt.GetComponent<Collider>();
+            var rElbow = rig.m_elbowRt.GetComponent<Collider>();       
+            Hand[] hands = [
+                    rig.leftHand,
+                    rig.rightHand,
+                ];
+            if (!modEnabled.Value /*&& DefaultPhysicsRigInfo.Setup*/) {
+                //ResetToHeptaRig(lShoulder, rShoulder, lElbow, rElbow, hands);
+                return;
+            }
             lShoulder.isTrigger = true;
             rShoulder.isTrigger = true;
             lElbow.isTrigger = true;
             rElbow.isTrigger = true;
             rig.manager.remapHeptaRig.jumpVelocity /= 1.3f;
-            Hand[] hands = new Hand[] {
-                    rig.leftHand,
-                    rig.rightHand,
-                };
             foreach (var hand in hands) {
                 float multiplier = 2f;
-                hand.physHand.armInternalMult *= multiplier;
-                hand.physHand._forceMultiplier *= multiplier - 0.5f;
+                hand.physHand._armInternalMult *= multiplier;
             }
         }
+        /*
+        private static void ResetToHeptaRig(Collider lShoulder, Collider rShoulder, Collider lElbow, Collider rElbow, Hand[] hands) {
+            lShoulder.isTrigger = DefaultPhysicsRigInfo.colliderTrigger;
+            rShoulder.isTrigger = DefaultPhysicsRigInfo.colliderTrigger;
+            lElbow.isTrigger = DefaultPhysicsRigInfo.colliderTrigger;
+            rElbow.isTrigger = DefaultPhysicsRigInfo.colliderTrigger;
+            foreach (var hand in hands) {
+                hand.physHand.armInternalMult = DefaultPhysicsRigInfo.armInternalMult;
+                //hand.physHand._forceMultiplier = DefaultPhysicsRigInfo.forceMultiplier;
+            }
+        }
+
+        public static class DefaultPhysicsRigInfo {
+            public static bool colliderTrigger = false;
+            public static float jumpVelocity = default;
+            public static float armInternalMult = default;
+            public static float forceMultiplier = default;
+            public static bool Setup {
+                get; private set;
+            } = false;
+            public static void SetupRigInfo(PhysicsRig rig){
+                if (Setup)
+                    return;
+                colliderTrigger = false;
+                jumpVelocity = rig.manager.remapHeptaRig.jumpVelocity;
+                Hand[] hands = [
+                    rig.leftHand,
+                    rig.rightHand,
+                ];
+                foreach (var hand in hands) {
+                    armInternalMult = hand.physHand.armInternalMult;
+                    forceMultiplier = hand.physHand._forceMultiplier;
+                }
+                Setup = true;
+            }
+        }
+                */
     }
 }
